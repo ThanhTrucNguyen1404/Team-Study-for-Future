19f5062aa2524c0f487510a8792bdd0f5ca8e342
Nguyễn Thị Thanh Trúc <anhkhoanguyenhoang14042004@gmail.com>
2025-03-04 15:00:00 +0700

    lab 02 - delete

--- Bundling and Minification - Optimized/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿// Function to change the background color of the body
-function changeBackgroundColor(color) {
-    document.body.style.backgroundColor = color;
-}
-
-// Function to display a welcome message
-function displayWelcomeMessage() {
-    alert('Welcome to the dummy JavaScript test!');
-}
--- Bundling and Monification - Not Optimized/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Bundling_and_Monification___Not_Optimized.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
